7169ad10663a786a6c5e8199600da08e9766c84b
Nguyen020905 <izneo666@gmail.com>
2025-09-28 05:28:35 +0700

    added tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ export default function TailwindRedTest() {
   return (
     <div className="min-h-screen flex items-center justify-center bg-red-500">
       <h1 className="text-6xl font-bold text-white shadow-lg p-8 rounded-xl">
-        Tailwind is Working!
+        Tailwind is NOT Working!
       </h1>
     </div>
   );
