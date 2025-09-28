c9f0e4b531001397fa5925a97e135d9526b832db
Luke Pham <vandenn.pham@gmail.com>
2025-09-27 17:32:57 -0700

    Update Hero.jsx

---
+++ Component/Hero.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Hero = () => {
+  return (
+    <div className="h-screen w-full bg-[#E1FFF0] flex text-center justify-centers bg-size[100%] flex-col items-center justify-center p-4">
+      <main className="text-center">
+        <h1 className="text-center text-6xl md:text-8xl font-bold ">
+          MOODLIFT
+        </h1>
+      </main>
+    </div>
+  );
+};
+
+export default Hero;
