56336641617839773a324a3884956d581cd33d01
Luke Pham <vandenn.pham@gmail.com>
2025-09-27 17:33:10 -0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import NavigationBar from "../Component/NavigationBar";
+import Hero from "../Component/Hero";
 
 function App() {
   return (
     <div>
       <NavigationBar />
+      <Hero />
       {/* Rest of your content */}
     </div>
   );
